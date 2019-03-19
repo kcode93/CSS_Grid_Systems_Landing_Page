6f8c8add82c9d6d991f65e09c61c94e3f7985a87
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-03-18 21:38:23 -0700

    added the javascript that will work for the visual effects on the  services.html

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,21 +1,4 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const container = document.querySelector('.container');
+$(document).ready(function(
 
-//adds and removes class hover-left when hovering hover
-left.addEventListener('mouseenter', () => {
-  container.classList.add("hover-left");
-});
 
-left.addEventListener('mouseleave', () => {
-  container.classList.remove("hover-left");
-});
-
-//adds and removes class hover-right when hovering hover
-right.addEventListener('mouseenter', () => {
-  container.classList.add("hover-right");
-});
-
-right.addEventListener('mouseleave', () => {
-  container.classList.remove("hover-right");
-});
+));//end of document
