7f0b6d6803baaba26baad661973b3279ebe231ac
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-05-01 22:29:29 -0700

    added google maps API to display a map of the company's location in the contact.html but did no include my API so map is not visible, yet the code does not give errors except for the missing key.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,25 @@
-$(document).ready(function(
+$(document).ready(function(){
+  var map;
+  var options;
+  options ={
+    center: {lat: 33.7701, lng: -118.1937},
+    zoom: 8
+  }
 
+  //crate MAP
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'),options);
+  }
 
-));//end of document
+  //add Marker
+  addMarker(33.8045,-118.1678);
+
+  //////////////////////////////////////////////////////////////////////////////
+  function addMarker(lat,lng){
+    var marker = new google.maps.Marker({
+      position:{lat: lat, lng: lng},
+      map:map
+    });
+  }
+
+});//end of document
